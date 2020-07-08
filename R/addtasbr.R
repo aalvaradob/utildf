05fa9877177d5fe7eccff2f514f8dfce6086b401
aalvaradob <alfredoalvarado@yahoo.com>
2020-07-08 18:08:19 -0400

    Including the "Others" in the addtasbr function
    Including the "Others" in the addtasbr function

--- R/addtasbr.R
+++ R/addtasbr.R
@@ -9,19 +9,27 @@
 addtasbr<-function(df,Rank,RankT,...){
   MyRank<-rlang::enquo(Rank)
   Vars1<-rlang::enquos(...)
-
+  #get the ones within the rank
   df1<-df%>%filter(!!MyRank<=RankT)
 
+  #get totals of top rank
+  df1T<-df1%>%
+    gettotalenq(...)
+
+  #get the other over the rank
   df2<-df%>%filter(!!MyRank>RankT)%>%
     gettotalenq(...)
 
+  #get the totals
   df3<-df%>%gettotalenq(...)
 
-  df3<-bind_rows(df1,df2,df3)
+  #add all tables
+  df3<-bind_rows(df1,df1T,df2,df3)
 
   nr<-df3%>%nrow()
 
-  df3[nr-1,1]<- "Sub-Total"
+  df3[nr-2,1]<- "Sub-Total"
+  df3[nr-1,1]<- "Others"
   df3[nr,1]<- "Total"
 
   df3
